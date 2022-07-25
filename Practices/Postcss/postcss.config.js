3ccc8cb4f22fedcc8a1deedd17da59e8a33c4e67
y00eunji <aine0227@naver.com>
2022-07-25 16:55:05 +0900

    PostCss 실습

---
+++ Practices/Postcss/postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: {
+    // cssnano: {},
+    "postcss-pxtorem": {},
+    "postcss-combine-media-query": {},
+    "postcss-import": {},
+    "postcss-preset-env": {
+      stage: 0,
+    },
+  },
+};
